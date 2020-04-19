3d69c6b053d8fb9ad4cade6138ab68f44123571e
Anton Afanassiev <antonafana@yahoo.ca>
2020-04-18 18:44:11 -0700

    Added code to hide the login button when user is logged in

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log(db);
+(function init() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			$('#jumbo-btn').hide();
+		} 
+	}, function(error) {
+		console.log(error);
+	});
+})();
